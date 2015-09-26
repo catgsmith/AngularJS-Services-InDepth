f2d5c0ac28f8a428c5d2d595eaeadd014345bf78
Catherine <catgsmith@hotmail.com>
2015-09-26 19:07:02 +0100

    add another setter to the booksProvider

--- public/app/app.js
+++ public/app/app.js
@@ -2,16 +2,24 @@
 
     var app = angular.module('app', []);
 
-    app.provider('books', function() {
+    app.provider('books', function() {        
         var name = 'Book Logger';
         var desc;
+        var includeVersionInTitle = false;
+        var version = "1.0";
 
         return {
           setDesc: function(value) {
             desc = value;
           },
+          setIncludeVersionInTitle: function (value) {
+            includeVersionInTitle = value;
+          },
         // PROVIRER COMES WITH GET FUNCTION
           $get: function() {
+            if (includeVersionInTitle) {
+                name += ' ' + version;
+            }
             return {
               appName: name,
               appDesc: desc
@@ -22,6 +30,7 @@
 
     app.config(function (booksProvider) {
       booksProvider.setDesc("Track which books you read.");
+      booksProvider.setIncludeVersionInTitle(true);
     });
 
 }());
